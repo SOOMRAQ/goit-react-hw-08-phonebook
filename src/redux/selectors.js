0e0c7a91891efe1818ffdf675542f9b8d6136967
Nikita Taranenko <soomraq.x@gmail.com>
2023-02-24 14:49:53 +0200

    Add auth selectors; preparing for creating Auth Slice

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,9 @@
 export const getContacts = state => state.contacts.items;
 export const getFilter = state => state.filter.value;
+
+// auth selectors
+
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectUserName = state => state.auth.user.name;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
+export const selectError = state => state.auth.error;
