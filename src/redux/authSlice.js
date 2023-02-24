1e8518af7fd59eecba5e055ddf34ae5eba337ea3
Nikita Taranenko <soomraq.x@gmail.com>
2023-02-24 15:08:34 +0200

    Add auth slice

---
+++ src/redux/authSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchCurrentUser, login, logout, register } from './thunks';
+
+const initialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: {
+    [register.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
+    [login.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
+    [logout.fulfilled](state) {
+      state.user = { name: null, email: null };
+      state.token = null;
+      state.isLoggedIn = false;
+    },
+    [fetchCurrentUser.pending](state) {
+      state.isRefreshing = true;
+    },
+    [fetchCurrentUser.fulfilled](state, action) {
+      state.user = action.payload;
+      state.isLoggedIn = true;
+      state.isRefreshing = false;
+    },
+    [fetchCurrentUser.rejected](state) {
+      state.isRefreshing = ture;
+    },
+  },
+});
+
+export const authReducer = authSlice.reducer;
